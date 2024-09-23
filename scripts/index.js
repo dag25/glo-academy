bf0a84a030db7e36d7bc115a3d070e61e013ce3a
dag <45577604+dag25@users.noreply.github.com>
2024-09-23 23:39:41 +0400

    feat: lesson03

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-const title = 'Курс "JavaScript"';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 1;
-const rollback = 100;
-const fullPrice = 150;
-const adaptive = true;
+let title = 'Курс "JavaScript"';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1;
+let rollback = 10;
+let fullPrice = 150;
+let adaptive = true;
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
@@ -18,5 +18,23 @@ console.log(screens.toLowerCase());
 console.log(screens.split(', '));
 console.log(fullPrice * (rollback / 100));
 
-
-
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать?');
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+console.log(Math.floor(servicePercentPrice));
+if (fullPrice > 30000) {
+	console.log('Даем скидку в 10%');
+} else if(fullPrice> 1500 && fullPrice < 30000) {
+	console.log('Даем скидку в 5%');
+} else if(fullPrice < 1500 && fullPrice > 0) {
+	console.log('Скидка не предусмотрена');
+} else if(fullPrice < 0) {
+	console.log('Что то пошло не так');
+}
