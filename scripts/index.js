e0e21280067bbb3b3904629b35ba8a1e7c13f14f
dag <45577604+dag25@users.noreply.github.com>
2024-09-24 23:44:03 +0400

    feat: lesson04

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,57 @@
-let title = 'Курс "JavaScript"';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1;
-let rollback = 10;
-let fullPrice = 150;
-let adaptive = true;
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(
-	`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`,
-);
-console.log(
-	`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`,
-);
-console.log(screens.toLowerCase());
-console.log(screens.split(', '));
-console.log(fullPrice * (rollback / 100));
-
-title = prompt('Как называется ваш проект?');
-screens = prompt('Какие типы экранов нужно разработать?');
-screenPrice = +prompt('Сколько будет стоить данная работа?');
-adaptive = confirm('Нужен ли адаптив на сайте?');
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
-console.log(Math.floor(servicePercentPrice));
-if (fullPrice > 30000) {
-	console.log('Даем скидку в 10%');
-} else if(fullPrice> 1500 && fullPrice < 30000) {
-	console.log('Даем скидку в 5%');
-} else if(fullPrice < 1500 && fullPrice > 0) {
-	console.log('Скидка не предусмотрена');
-} else if(fullPrice < 0) {
-	console.log('Что то пошло не так');
+
+let rollback = 10;
+let fullPrice;
+const servicePercentPrice = fullPrice - (fullPrice * (rollback / 100));
+let allServicePrices;
+
+const showTypeOf = function(variable) {
+	console.log(variable, typeof variable);
 }
+
+const getRollbackMessage = function(price) {
+	if (price > 30000) {
+		return 'Даем скидку в 10%';
+	} else if (price >= 1500 && price < 30000) {
+		return 'Даем скидку в 5%';
+	} else if (price < 1500 && price >= 0) {
+		return 'Скидка не предусмотрена';
+	} else {
+		return 'Что то пошло не так';
+	}
+};
+
+const getAllServicePrices = function(servicePrice1, servicePrice2) {
+	return servicePrice1 + servicePrice2;
+};
+
+const getFullPrice = function(screenPrice, allServicePrices) {
+	return screenPrice + allServicePrices;
+};
+
+const getTitle = function(title) {
+	return title.trim().charAt(0).toUpperCase() + string.slice(1)
+};
+
+const  getServicePercentPrices = function(fullPrice, rollback) {
+	return fullPrice - rollback;
+};
+
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(screens);
+console.log(`${getRollbackMessage(25000)}`);
+console.log(`Стоимость составит ${getServicePercentPrices(fullPrice, rollback)}`);
